64fe413beb8983510f2a6db0685a72ea1b796549
A1iBA <counterstrikemab@gmail.com>
2025-10-10 16:58:06 -0400

    small functions to implement

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
+import java.time.LocalTime;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
 
         RouteCatalogue routeCatalogue = RouteCatalogue.getInstance();
         ConnectionCatalogue connectionCatalogue = ConnectionCatalogue.getInstance();
+    }
+    public void sort(ArrayList<Connection> Connectionslist){
+
+    }
+    public ArrayList<Connection> search(String RouteID, LocalTime departureTime, LocalTime arrivalTime, String departureCity,
+                                        String arrivalCity, String trainType, float firstClassTicket, float secondClassTicket, String daysofOperation){
+
     }
 }
